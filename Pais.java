e412c3ee613957bef211f54730b6eee86fb41d66
gi-a-dias <giovannaalmeidasdias@gmail.com>
2025-03-24 08:15:03 -0300

    Add files via upload

---
+++ Pais.java
@@ -0,0 +1,15 @@
+public class Pais {
+    
+    protected String sigla;
+    protected String pais;
+    protected int qtdeTitulos;
+    protected int qtdeShows;
+    protected int qtdeFilmes;
+    protected double tarifaBasica;
+    protected double tarifaStandard;
+    protected double tarifaPremium;
+
+    //construtor
+    
+
+}
